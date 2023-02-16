f45f831b5f3a1305ad4e203e4b0de146c37c0a63
niklashans <104555311+niklashans@users.noreply.github.com>
2023-02-16 13:51:03 +0100

    Update to video plyr

--- dist/startup-academy/videos.js
+++ dist/startup-academy/videos.js
@@ -1,9 +1,1 @@
-"use strict";
-(() => {
-  // bin/live-reload.js
-  new EventSource(`http://localhost:${3e3}/esbuild`).addEventListener(
-    "change",
-    () => location.reload()
-  );
-})();
-//# sourceMappingURL=videos.js.map
+"use strict";(()=>{})();
